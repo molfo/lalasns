c9e62a92822e6dd0bc77fe3b3fce49f27df2646d
molfo <miyabi.miyabiwa@gmail.com>
2021-11-23 19:48:42 +0900

    indexによるタスク表示機能を実装

---
+++ resources/views/app.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+        <title>Laravel CRUD</title>
+    </head>
+    <body>
+        <h1>Todos</h1>
+        <hr>
+        
+        <h2>Add new task</h2>
+        <hr>
+
+        <h2>Pending tasks</h2>
+        <ul class="list-group">
+            @foreach($todos as $todo)
+                <li class="list-group-item">{{$todo->task}}</li>
+            @endforeach
+        <hr>
+
+        <h2>Completed Tasks</h2>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+    </body>
+</html>
